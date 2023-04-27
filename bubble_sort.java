d190ca8be0a70d9bf7d537bfde1781cb2b053cef
Hetvi Patel <hetvi.patel.585112@gmail.com>
2023-04-27 12:16:53 +0530

    first level sorting

--- bubble_sort.java
+++ bubble_sort.java
@@ -3,11 +3,14 @@ class Bubble_sort{
   public static void bubbleSort(int[] nums) {
 
     int temp = 0;
-
-    temp = nums[1];
-    nums[1] = nums[2];
-    nums[2] = temp;
-    
+    int n = nums.length;
+    for(int i=0; i < n-1; i++) {
+      if(nums[i] > nums[i+1] ) {
+        temp = nums[i];
+        nums[i] = nums[i+1];
+        nums[i+1] = temp;
+      }
+    }
   }
 
   public static void printArre(int[] nums){
@@ -18,7 +21,7 @@ class Bubble_sort{
 
     public static void main(String[] args) {
         
-        int[] myNum = {10, 20, 30, 40};
+        int[] myNum = {40, 20, 30};
         System.out.println("Array before bubble sort:");
         printArre(myNum);
         bubbleSort(myNum);

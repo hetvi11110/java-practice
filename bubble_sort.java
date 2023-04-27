6c3cc2eadaf8433de02a50a80e31af09f62ac0d1
Hetvi Patel <hetvi.patel.585112@gmail.com>
2023-04-27 11:56:46 +0530

    first swap

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,18 +1,29 @@
 class Bubble_sort{
 
- 
-
   public static void bubbleSort(int[] nums) {
+
+    int temp = 0;
+
+    temp = nums[1];
+    nums[1] = nums[2];
+    nums[2] = temp;
     
   }
 
+  public static void printArre(int[] nums){
+    for (int i : nums) {
+      System.out.println(i);
+    }
+  }
+
     public static void main(String[] args) {
-        System.out.println("Array before bubble sort");
+        
         int[] myNum = {10, 20, 30, 40};
-        for (int i : myNum) {
-          System.out.println(i);
-        }
+        System.out.println("Array before bubble sort:");
+        printArre(myNum);
         bubbleSort(myNum);
+        System.out.println("Array after bubble sort:");
+        printArre(myNum);
       }
 
 }

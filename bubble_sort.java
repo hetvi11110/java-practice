daa60d741c0a6646384a251da86be5f463ab43ff
Hetvi Patel <hetvi.patel.585112@gmail.com>
2023-04-27 11:27:58 +0530

    print array

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,7 +1,18 @@
 class Bubble_sort{
 
+ 
+
+  public static void bubbleSort(int[] nums) {
+    
+  }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Array before bubble sort");
+        int[] myNum = {10, 20, 30, 40};
+        for (int i : myNum) {
+          System.out.println(i);
+        }
+        bubbleSort(myNum);
       }
 
 }

f6e635bb094ec09657bfccf30a7580f4940d3d96
Hetvi Patel <hetvi.patel.585112@gmail.com>
2023-04-29 20:36:49 +0530

    SLL delet node start

--- single-linked-list/SLL.java
+++ single-linked-list/SLL.java
@@ -14,15 +14,12 @@ class SLL {
     Node head = null;
 
     void firstAdd(String ndata){
-
         Node newNode = new Node(ndata);
         newNode.next = head;
         head = newNode;
-
     }
 
     void lastAdd(String ndata){
-
         Node newNode = new Node(ndata);
         Node currentNode = head;
         while(currentNode.next != null){               
@@ -31,17 +28,27 @@ class SLL {
         currentNode.next = newNode;
     }
 
-    void display(){
-
+    void deleteNode(String ndata){
         Node currentNode = head;
+        while(currentNode.next != null){  
+            if(currentNode.data == ndata) {
+                if(currentNode == head){
+                    
+                }
+                break;
+            }else {             
+                currentNode = currentNode.next;
+            }
+        }
+    }
 
+    void display(){
+        Node currentNode = head;
         while(currentNode != null){
             System.out.println(currentNode.data);
             currentNode = currentNode.next;
         }
-
     }
-
     
     public static void main(String[] args) {
 
@@ -51,6 +58,7 @@ class SLL {
         obj.firstAdd("Patel");
         obj.lastAdd("Ankit");
         obj.display();
+        obj.deleteNode("Patel");
         
     }
 
